7a12f5e5507b69ca7f91587c0ec57d7667536554
InesRobat <inesrobat@outlook.com>
2025-02-07 09:40:22 -0800

    :bug:

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: "frontend/dist",
-  publicPath: "./",
+  publicPath: "/",
   productionSourceMap: false,
 });
